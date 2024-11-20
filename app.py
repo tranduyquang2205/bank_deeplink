540210cdb42e8dc4f897dce5045647b3a2e528df
Trần Duy Quang <tranduyquang2205@gmail.com>
2024-11-20 09:38:39 +0700

    update

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ import sys
 import traceback
 from api_response import APIResponse
 from api import generate_deep_link
+import json
+
 app = FastAPI()
 @app.get("/")
 def read_root():
@@ -26,7 +28,24 @@ def login_api(input: Transfer_Information):
         response = str(e)
         print(traceback.format_exc())
         print(sys.exc_info()[2])
-        return APIResponse.json_format(response)
-    
+        return APIResponse.json_format(response)\
+
+
+@app.get('/banks_list', tags=["bank_list"])
+def bank_list():
+    try:
+        # Load the banks.json file
+        with open("banks.json", "r", encoding="utf-8") as file:
+            bank_data = json.load(file)
+        
+        # Extract the list of banks
+        bank_list = bank_data.get("data", [])
+        
+        return APIResponse.json_format(bank_list)
+    except Exception as e:
+        # Handle and log errors
+        error_message = f"Error loading banks list: {str(e)}"
+        print(traceback.format_exc())
+        return APIResponse.json_format({"error": error_message}, status_code=500)
 if __name__ == "__main__":
     uvicorn.run(app ,host='0.0.0.0', port=3000)
